eb1e0d4011da2f41347cdc0a4e80985e7cffa3ba
Elizabeth <elizabethgran12@gmail.com>
2023-09-14 14:54:49 -0500

    add pylint run to build

--- inflammation/models.py
+++ inflammation/models.py
@@ -34,3 +34,8 @@ def daily_min(data: object) -> object:
     """
     return np.min(data, axis=0)
 
+
+def patient_normalise(data):
+    """Normalise patient data from a 2D inflammation data array"""
+    max_data = np.max(data, axis=1)
+    return data / max_data[:, np.newaxis]
--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,8 @@
 import numpy as np
 import numpy.testing as npt
 import pytest
-from inflammation.models import daily_mean, daily_min, daily_max
+from inflammation.models import daily_mean, daily_min, daily_max, patient_normalise
+
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -52,6 +53,7 @@ def test_daily_min():
     npt.assert_array_equal(daily_min(test_input), test_result)
 
 
+'''
 @pytest.mark.parametrize(
     "test, expected",
     [
@@ -61,3 +63,15 @@ def test_daily_min():
 def test_daily_mean(test, expected):
     """Test mean function works for array of zeroes and positive integers"""
     npt.assert_array_equal(daily_mean(test), expected)
+'''
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+
+     ])
+def test_patient_normalise(test, expected):
+    """Test normalisation works for arrays of one and positive integers,
+    Assumption that test accuracy of two decimal places is sufficient"""
+    npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
